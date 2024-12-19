ec5407cbc941efd372a115ca9d6f3f28d85ecdc3
Yankie <60907574+Cradber@users.noreply.github.com>
2024-12-19 16:55:09 +0100

    Adding Debug serialization

--- serialization/nlohmann/debugserialization.h
+++ serialization/nlohmann/debugserialization.h
@@ -1,4 +1,29 @@
 #ifndef DEBUGSERIALIZATION_H
 #define DEBUGSERIALIZATION_H
 
+#include "models/models.h"
+#include "serialization/nlohmann/jsonserializernlohmann.h"
+
+
+class DebugSerializer : public JsonSerializerNlohmann<Debug> {
+public:
+    Debug from_json(const json& j) {
+        Debug debug;
+
+        debug.set(j.at("sync").get<std::string>());
+
+        return debug;
+    }
+
+    json to_json(const Debug& debug) {
+        auto j = json::object();
+
+        j["sync"] = debug.get();
+
+        return j;
+    }
+
+};
+
+
 #endif // DEBUGSERIALIZATION_H
